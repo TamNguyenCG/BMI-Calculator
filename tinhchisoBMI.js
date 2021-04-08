3bded26d0d55f6f39baaf3e68006401910c35afe
TamNguyenCG <ngductam91@gmail.com>
2021-04-08 22:00:52 +0700

    updated

--- tinhchisoBMI.js
+++ tinhchisoBMI.js
@@ -1,17 +1,13 @@
-let a=prompt("Nhập cân nặng")
-let b=prompt("Nhập chiều cao")
-BMI=a/(b*b);
-if (BMI >= 18.5) {
-    alert('Bạn hơi gầy')
-} else if (18.5 <= BMI&&BMI <= 24.9) {
-    alert('Bình thường')
-}else if (25 <= BMI&&BMI  <= 29.9) {
-    alert('Bạn hơi mập')
-}else if (30 <=BMI&&BMI  <= 34.9) {
-    alert('Bạn béo cấp 1')
-}else if (35 <= BMI&&BMI  <= 39.9) {
-    alert('Bạn béo cấp 2')
+let w=prompt("Enter your weight (kg)")
+let h=prompt("Enter your height (m)")
+bmi=w/(h*h);
+if (bmi<18.5){
+    alert('Your BMI is '+bmi+ ' Underweight')
+}else if(bmi<25){
+    alert('Your BMI is '+bmi+ ' Normal')
+}else if(bmi<30){
+    alert('Your BMI is '+bmi+ ' Overweight')
 }else{
-    alert('Nguy hiểm !!! Bạn nên chú ý sức khỏe')
+    alert('Your BMI is '+bmi+ ' Dangerous !')
 }
 
